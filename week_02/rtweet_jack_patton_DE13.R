ce83efad5bfb4dcef1d1ae049ce8a51efbeca3a3
Arctic-64 <77393546+Arctic-64@users.noreply.github.com>
2022-03-16 09:57:48 +0000

    Create rtweet_jack_patton_DE13.R

---
+++ week_02/rtweet_jack_patton_DE13.R
@@ -0,0 +1,7 @@
+setwd("D:/UNIVERSITY/CodeClan/de13_classnotes/week_02/day_2/4_homework/data/")
+cc_info = read.csv(file = "code_clan_info.csv")
+cc_tweets = read.csv(file = "code_clan_tweets.csv")
+
+summary(cc_tweets)
+summary(cc_info)
+
